6938b292005c2120a1dd1bac6fcc07a776edb732
AlainArnault <alain.arnault89@gmail.com>
2024-09-30 19:17:07 +0200

    modif fonctions + bouclage infini

--- assets/script.js
+++ assets/script.js
@@ -17,19 +17,50 @@ const slides = [
 	}
 ]
 
+for (let i = 0; i < slides.length; i++) {
+	let point = document.createElement("li");
+	let listePoint = document.getElementById("bullet_point");
+	point.classList.add("dot");
+	listePoint.appendChild(point)
+}
+
+// Fonction mise à jour des points
+function majPoint (index) {
+	let pointCourant = document.querySelectorAll(".dot");
+	pointCourant.forEach((dot, i) => {
+		if (i === index) {
+			dot.classList.add("dot_selected")
+		} else {
+			dot.classList.remove("dot_selected")
+		}
+	});
+}
+
+// Fonction mise à jour du slider
+function majSlider (index) {
+	let imgSLider = document.getElementById("imgSlider");
+	imgSLider.setAttribute("src",  "./assets/images/slideshow/"+slides[index].image);
+	let tagLineSlider = document.getElementById("tagLineSlider");
+	tagLineSlider.innerHTML = slides[index].tagLine;
+	majPoint(index)
+}
+
+//Définition de l'index de la slide courante
+let indexCourant = 0
+
+//Mise à 0 des Slides
+majSlider(indexCourant);
+
+
+// Gestion des flèches avec mie à jour de l'index à chaque tour et bouclage à l'infini avec opérateur modulo
 let flecheGauche = document.getElementById("flecheGauche");
 flecheGauche.addEventListener("click", function () {
-	console.log ("clic gauche")
+	indexCourant = (indexCourant - 1 + slides.length) % slides.length;
+	majSlider(indexCourant)
 })
 
 let flecheDroite = document.getElementById("flecheDroite");
 flecheDroite.addEventListener("click", function () {
-	console.log ("clic droite")
+	indexCourant = (indexCourant + 1 + slides.length) % slides.length;
+	majSlider(indexCourant)
 })
-
-let bulletPoint = document.querySelector(".dots");
-let nbBulletPoint = slides.length;
-
-let li = document.createElement("li")
-let ul = document.getElementById("bullet_point")
-ul.appendChild(li)
